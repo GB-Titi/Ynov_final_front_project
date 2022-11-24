5fc16bf285e26465f40f61cdee639620ccda6bb7
gb-titi <tristanmatteoli@gmail.com>
2022-11-24 14:30:30 +0100

    Après création des composants et page necessaires de l'application, et implémentation des composants de base dans l'index. Il faudra les modifier pour correspondre à ce que l'on souhaite

---
+++ gamos_app/components/MenuComponent.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const MenuComponent: React.FC = () => {
+    return (
+        <Head>
+            <title>Gamos Application</title>
+            <meta name="description" content="Bienvenue sur l'application Gamos !" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+  };
+
+export default MenuComponent;
