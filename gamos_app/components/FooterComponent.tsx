530bc02879b2a472f1b52a02d6934a718b6b39d5
gb-titi <tristanmatteoli@gmail.com>
2022-11-25 15:18:50 +0100

    modif page connexion

--- gamos_app/components/FooterComponent.tsx
+++ gamos_app/components/FooterComponent.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 const FooterComponent: React.FC = () => {
     return (
         <footer className={styles.footer}>
-            <a className={styles.footer_link}>Contact</a>
-            <a className={styles.footer_link}>Conditions général des ventes</a>
-            <span className={styles.footer_span}>RIDE 2022 - tout droits réservés</span>
+            <a className={styles.link}>Contact</a>
+            <a className={styles.link}>Conditions général des ventes</a>
+            <span className={styles.span}>RIDE 2022 - tout droits réservés</span>
         </footer>
     );
   };
