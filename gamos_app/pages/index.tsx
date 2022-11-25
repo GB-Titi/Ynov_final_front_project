12af6e5fd5e4332f680a2e5730feff6e29ad5d78
tom-ruiz <tomruiz2611@gmail.com>
2022-11-25 15:23:56 +0100

    page accueil OK

--- gamos_app/pages/index.tsx
+++ gamos_app/pages/index.tsx
@@ -11,24 +11,62 @@ export default function Home() {
           <div className={styles.infos}>
             <span>▷</span> Depuis 2008, RIDE, agence de location de voitures de luxe propose ses services partout en France (Paris, Monaco, Nice, Cannes, Saint-Tropez, Courchevel, Saint-Moritz...).
             <br />Notre expérience est à votre service pour répondre à toutes vos demandes
+            <hr />
           </div>
 
         </div>
         <div className={styles.container}>
           <form action="POST" className={styles.form}>
-            <h2>INSCRIPTION</h2>
+            <h1>INSCRIPTION</h1>
             <p>Je suis :</p>
             <div className={styles.radioContainer}>
-              <div>
-                <input className={styles.radio} type="radio" id="huey" name="drone" value="huey" checked />
-                  <label>Une entreprise</label>
-              </div>
-
-              <div>
-                <input className={styles.radio}  type="radio" id="dewey" name="drone" value="dewey" />
-                  <label>Un particulier</label>
-              </div>
+              <label className={styles.radio}>
+                <input name="radio" type="radio" checked />
+                <span>une entreprise</span>
+              </label>
+              <label className={styles.radio}>
+                <input name="radio" type="radio" />
+                <span>un particulier</span>
+              </label>
             </div>
+            <div className={styles.fieldContainer}>
+              <label>
+                <span>Nom</span>
+                <input type="text" />
+                <div className={styles.border}></div>
+              </label>
+              <label>
+                <span>Prénom</span>
+                <input type="text" />
+                <div className={styles.border}></div>
+              </label>
+
+              <label>
+                <span>E-mail</span>
+                <input type="email" />
+                <div className={styles.border}></div>
+              </label>
+
+              <label>
+                <span>Numéro de téléphone</span>
+                <input type="tel" />
+                <div className={styles.border}></div>
+              </label>
+
+              <label>
+                <span>Nationalité</span>
+                <select>
+                  <option value="">Français</option>
+                  <option value="">Pas français</option>
+                </select>
+                <div className={styles.border}></div>
+              </label>
+            </div>
+            <label className={styles.checkbox}>
+              <input name="check" type="checkbox" />
+              <span>j’atteste que je possède un permis de conduire valide.</span>
+            </label>
+            <div className={styles.button}>Demander mon inscription</div>
           </form>
         </div>
       </main>
