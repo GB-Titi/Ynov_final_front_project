b5fd4380285347cf0a0d75fb58ef5934abf74a37
sam, here <70657869+samir-massoundi@users.noreply.github.com>
2022-11-25 15:03:12 +0100

    the last commit before we merge with main

--- gamos_app/pages/index.tsx
+++ gamos_app/pages/index.tsx
@@ -13,6 +13,7 @@ export default function Home() {
             <span>▷</span> Depuis 2008, RIDE, agence de location de voitures de luxe propose ses services partout en France (Paris, Monaco, Nice, Cannes, Saint-Tropez, Courchevel, Saint-Moritz...).
             <br />Notre expérience est à votre service pour répondre à toutes vos demandes
           </div>
+          <LoginButtonComponent>wdflkf</LoginButtonComponent>
 
         </div>
         <div className={styles.container}>
