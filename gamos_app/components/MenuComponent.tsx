654a551914a12e68bcf0bedab6d604126964764c
GB-Titi <tristanmatteoli@gmail.com>
2022-11-26 16:54:32 +0100

    Ajout du bonton fonctionnel mais problème de css. Ajout de babel pour essayer de rectifier le pb mais rien à faire ça marche pas

--- gamos_app/components/MenuComponent.tsx
+++ gamos_app/components/MenuComponent.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/components/Menu.module.scss";
 import logo from "../public/Logo.svg"
 import Image from "next/image"
 import Link from 'next/link'
+import { LinkButtonComponent, ButtonComponent } from "my-lib-ui";
 
 const MenuComponent: React.FC = () => {
   return (
@@ -10,7 +11,10 @@ const MenuComponent: React.FC = () => {
         <Image className={styles.logo} src={logo} alt="logo" width={66} height={66} />
         <span className={styles.brand_name}>RIDE</span>
       </div>
-      <Link className={styles.button} href="/login">Connexion admin</Link>
+      {/* <Link href="/login">
+        <ButtonComponent text="Connexion"></ButtonComponent>
+      </Link> */}
+      <LinkButtonComponent href="login" text="Connexion"/>
     </div>
   );
 };
