e1f5dcab7f2e25c7e464a532a3769371e18a5fd6
GB-Titi <tristanmatteoli@gmail.com>
2022-12-01 23:17:57 +0100

    modifs en speed

--- gamos_app/components/LoginFormComponent.tsx
+++ gamos_app/components/LoginFormComponent.tsx
@@ -1,11 +1,16 @@
 import styles from "../styles/components/LoginForm.module.scss";
 import Link from 'next/link'
+import red_arrow from "../public/arrow.png"
+import Image from "next/image"
 import { LinkButtonComponent, InputComponent } from "my-lib-ui";
 
 const LoginFormComponent: React.FC = () => {
   return (
     <div className={styles.login_container}>
-         <Link href="/" className={styles.backlink}>retour</Link>
+      <div className={styles.backlink_field}>
+        <Image src={red_arrow} alt="logo" width={12} />
+        <Link href="/" className={styles.backlink}>retour</Link>
+      </div>
         <form>
             <h1>Connexion</h1>
             <InputComponent placeholder="Username" type="text"/>
--- gamos_app/pages/register_confirmation.tsx
+++ gamos_app/pages/register_confirmation.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 // import { useRouter } from "next/router";
 import FooterComponent from "../components/FooterComponent";
 import MenuComponent from "../components/MenuComponent";
-import { LinkButtonComponent } from "my-lib-ui";
+import merchant_center from "../public/btn_store.png"
+import Image from "next/image"
+import { LinkButtonComponent, InputComponent,RadioButtonComponent, SelectCountryComponent, CheckboxComponent } from "my-lib-ui";
 // import LoginFormComponent from "../components/LoginFormComponent";
 // import Link from 'next/link';
 import styles from "../styles/components/Home.module.scss"
@@ -10,14 +12,28 @@ import styles from "../styles/components/Home.module.scss"
 const Register_conf: NextPage = () => {
     //   const router = useRouter();
     return (
-        <div className={styles.container}>
-            <MenuComponent />
-            <main>
-                <span>Inscription confirmée</span>
-                <LinkButtonComponent href="/" text="Retour à l'accueil"/>
-            </main>
-            <FooterComponent />
-        </div>
+        <div>
+        <MenuComponent />
+        <main>
+          <div className={styles.background}>
+            <div className={styles.infos}>
+              <span>▷</span> Depuis 2008, RIDE, agence de location de voitures de luxe propose ses services partout en France (Paris, Monaco, Nice, Cannes, Saint-Tropez, Courchevel, Saint-Moritz...).
+              <br />Notre expérience est à votre service pour répondre à toutes vos demandes
+              <hr />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.form}>
+            <h1>INSCRIPTION</h1>
+              <div className={styles.confirmation_content}>
+                <span>Votre demande d’inscription a bien été prise en compte.<br />Vous allez recevoir une confirmation par mail, en attendant vous pouvez télécharger l’application.</span>
+              <Image className="image_store" src={merchant_center} alt="merchant_center"/>
+              </div>
+            </div>
+          </div>
+        </main>
+        <FooterComponent />
+      </div>
     );
 };
 
