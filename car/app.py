596952a534ccf2d8ffc9a3c9b7b3bf8f4fd5aea7
sam, here <70657869+samir-massoundi@users.noreply.github.com>
2023-01-24 16:09:59 +0100

    updated with tristan's files

--- car/app.py
+++ car/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify, request
 import json
-
-app = Flask(__name__)
+ 
+app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<db_name>.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+ 
+@app.before_first_request
+def create_table():
+    db.create_all()
+ 
+app.run(host='localhost', port=5000)
 
 @app.route("/", methods=["GET"])
 def hello():
@@ -9,5 +18,20 @@ def hello():
 
 @app.route("/add", methods=['POST'])
 def add():
+    data = json.loads(request.data)
+    return jsonify(data["a"] + data["b"])
+
+@app.route("/remove", methods=['POST'])
+def remove():
+    data = json.loads(request.data)
+    return jsonify(data["a"] + data["b"])
+
+@app.route("/car", methods=['GET'])
+def car():
+    data = json.loads(request.data)
+    return jsonify(data["a"] + data["b"])
+
+@app.route("/update", methods=['POST'])
+def update():
     data = json.loads(request.data)
     return jsonify(data["a"] + data["b"])
