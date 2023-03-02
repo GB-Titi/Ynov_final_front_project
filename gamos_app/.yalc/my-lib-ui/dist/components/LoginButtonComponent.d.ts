9a302560ee57d866532212684672c730b6876fd4
tom-ruiz <tomruiz2611@gmail.com>
2023-03-02 10:07:24 +0100

    ok

---
+++ gamos_app/.yalc/my-lib-ui/dist/components/LoginButtonComponent.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+declare type LoginButtonComponentProps = {
+    children?: string;
+};
+declare const LoginButtonComponent: React.FC<LoginButtonComponentProps>;
+export default LoginButtonComponent;
---
+++ gamos_app/.yalc/my-lib-ui/dist/stories/LoginButtonComponent.stories.d.ts
@@ -0,0 +1,5 @@
+import { ComponentStory } from "@storybook/react";
+import LoginButtonComponent from "../components/LoginButtonComponent";
+declare const _default: import("@storybook/csf").ComponentAnnotations<import("@storybook/react").ReactFramework, any>;
+export default _default;
+export declare const CheckboxComponentView: ComponentStory<typeof LoginButtonComponent>;
